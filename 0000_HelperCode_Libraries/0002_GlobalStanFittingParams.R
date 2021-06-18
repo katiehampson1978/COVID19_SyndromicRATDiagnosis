14e5d6939357e053206b91c1ba17ecaabb11a038
Fergus J Chadwick <fergusjchadwick@gmail.com>
2021-06-19 00:11:17 +0100

    Code out iterations to use defaults

--- 0000_HelperCode_Libraries/0002_GlobalStanFittingParams.R
+++ 0000_HelperCode_Libraries/0002_GlobalStanFittingParams.R
@@ -4,7 +4,7 @@ N_Chains <- 2
 # restrict the initial range to limit numerical overflow (inits being rejected)
 Init_R <- 0.1
 # iter includes warmup - default is half of iter will be warmup
-Iter <- 2000
+# Iter <- 1000
 # improve mixing
 # Think of like step length in Gibbs, this will slow things down
 AdaptDelta <- 0.99
