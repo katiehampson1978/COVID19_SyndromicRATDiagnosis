fda5c74c02638838851cfd78335021d802ade186
Fergus J Chadwick <fergusjchadwick@gmail.com>
2021-06-20 15:06:33 +0100

    Added dtplyr and MLmetrics to library file

--- 0000_HelperCode_Libraries/0001_Libraries.R
+++ 0000_HelperCode_Libraries/0001_Libraries.R
@@ -1,5 +1,9 @@
 library(cmdstanr) # Stan interface
+library(dtplyr) # Data table speed with dplyr syntax
 library(dummies) # Easy creation of dummy variables
 library(furrr) # For parallelised functional programming
+library(MLmetrics) # Log Loss/Cross Entropy calculation
 library(posterior) # Easy manipulation of posterior samples
 library(tidyverse) # General data manipulation
+
+
