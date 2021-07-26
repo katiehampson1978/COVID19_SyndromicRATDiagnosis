8d04cf393261792148bee016c5b94e25c095c0ba
fergusjchadwick <fergusjchadwick@gmail.com>
2021-07-26 13:34:34 +0100

    Rerun analysis on round 2 data

--- 0100_Data/0101_population_summary.R
+++ 0100_Data/0101_population_summary.R
@@ -2,6 +2,7 @@
 source("0000_HelperCode_Libraries/0001_Libraries.R")
 # Import Data
 dat <- readRDS("0100_Data/0103_nasal_dat.RDS")
+
 # Calculate population characteristics
 summary <- dat %>% 
   group_by(gender) %>% 
