9394179750d09f0618fedccb05c24cd01a46266d
fergusjchadwick <fergusjchadwick@gmail.com>
2021-07-27 14:32:37 +0100

    Saved population summary for automatic knitting

--- 0100_Data/0101_population_summary.R
+++ 0100_Data/0101_population_summary.R
@@ -4,9 +4,11 @@ source("0000_HelperCode_Libraries/0001_Libraries.R")
 dat <- readRDS("0100_Data/0103_nasal_dat.RDS")
 
 # Calculate population characteristics
-summary <- dat %>% 
+pop_summ <- dat %>% 
   group_by(gender) %>% 
   summarise("Gender_Count" = summary(gender),
             "Age_Mean" = mean(age),
             "Age_SD" = sd(age)) %>%
   filter(!Gender_Count == 0) # Remove annoying duplications
+
+saveRDS(pop_summ, "0100_Data/0103_pop_summ.RDS")
